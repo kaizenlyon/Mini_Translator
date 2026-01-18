b825af445d6ead49e9e7eb830c06f568d2f71d46
kaizenlyon <itskaizenlyon@gmail.com>
2026-01-18 11:30:52 +0300

    streamlit app

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
+from translator import translate
+from explainer import explain
 
 st.title("Mini-Übersetzer mit Erklärungen")
 
-text = st.text_area("Text eingeben")
+text = st.text_area("Text eingeben", height=150)
 
 if st.button("Übersetzen"):
-    st.write("Übersetzung kommt hier")
-    st.write("Erklärung kommt hier")
+    translation = translate(text)
+    explanation = explain(text, translation)
+
+    st.subheader("Übersetzung")
+    st.write(translation)
+
+    st.subheader("Erklärung")
+    st.write(explanation)
+
